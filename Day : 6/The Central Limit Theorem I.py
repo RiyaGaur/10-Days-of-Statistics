035f81c9acb6ae3bce43be753d9447d5bf8debba
RiyaGaur <44978076+RiyaGaur@users.noreply.github.com>
2020-06-23 21:22:02 +0530

    Create The Central Limit Theorem I.py

---
+++ Day : 6/The Central Limit Theorem I.py
@@ -0,0 +1,25 @@
+# 1.The Central Limit Theorem I
+
+
+<h2>Objective</h2>
+In this challenge, we practice solving problems based on the Central Limit Theorem. 
+
+<h2>Task</h2>
+A large elevator can transport a maximum of 9800 pounds. Suppose a load of cargo containing 49 boxes must be transported via the elevator. The box weight of this type of cargo follows a distribution with a mean of meu=205 pounds and a standard deviation of sigma=15 pounds. Based on this information, what is the probability that all 49 boxes can be safely loaded into the freight elevator and transported?
+
+<h2>Input Format</h2>
+
+There are 4 lines of input (shown below):
+
+9800<br>
+49<br>
+205<br>
+15
+
+The first line contains the maximum weight the elevator can transport. The second line contains the number of boxes in the cargo. The third line contains the mean weight of a cargo box, and the fourth line contains its standard deviation.
+
+If you do not wish to read this information from stdin, you can hard-code it into your program.
+
+<h2>Output Format</h2>
+
+Print the probability that the elevator can successfully transport all 49 boxes, rounded to a scale of 4 decimal places (i.e.,1.2345 format).
